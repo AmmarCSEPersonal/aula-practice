a2d61a42884425be071d3a1100fe933b5680b5a8
Unknown <ammar.cse@gmail.com>
2018-11-02 22:00:53 +0200

    add get rectifications api test

---
+++ rectification-service/api/spec/api.js
@@ -0,0 +1,20 @@
+import server from '../../';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('api', () => {
+  describe('GET /rectifications', () => {
+    it('responds with OK', done => {
+      chai.request(server)
+        .get('/rectifications')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          done();
+        });
+      });
+  });
+});
